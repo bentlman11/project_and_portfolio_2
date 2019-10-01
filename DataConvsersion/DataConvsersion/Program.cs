ca16bd4d7857398ff073e06878ba0667e5aa89a0
LT_Ben <bentlman1981@gmail.com>
2019-10-01 14:11:41 -0700

    Started Data Conversion
    Program and Validation started

---
+++ DataConvsersion/DataConvsersion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DataConvsersion
+{
+    class Program
+    {
+        private static Program _instance;
+
+        static void Main(string[] args)
+        {
+            //Database Location
+            //string cs = @"server= 127.0.0.1;userid=root;password=root;database=SampleRestaurantDatabase;port=8889";
+            //Output Location
+            //string _directory = @"../../output/";﻿﻿
+
+            _instance = new Program();
+
+            bool programIsRunning = true;
+
+            while(programIsRunning)
+            {
+                _instance.MainMenu();
+
+                string selection = Validation.GetString("Please enter your selection: ").ToLower();
+            }
+            
+        }
+
+        private void MainMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Hello Admin, What Would You Like To Do Today?");
+            Console.WriteLine("");
+            Console.WriteLine("1. Convert The Restaurant Profile Table From SQL To JSON");
+            Console.WriteLine("2. Showcase Our 5 Star Rating System");
+            Console.WriteLine("3. Showcase Our Animated Bar Graph Review System");
+            Console.WriteLine("4. Play A Card Game");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("");
+
+        }
+
+        private void WaitForKey()
+        {
+            Console.WriteLine("Press Enter To Continue");
+        }
+    }
+}
---
+++ DataConvsersion/DataConvsersion/Validation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataConvsersion
+{
+    class Validation
+    {
+        public static string GetString(string message = "Enter your selection: ")
+        {
+            string valString = null;
+
+            do
+            {
+                Console.WriteLine(message);
+                valString = Console.ReadLine();
+            }
+            while (String.IsNullOrWhiteSpace(valString));
+
+            return valString;
+        }
+    }
+}
